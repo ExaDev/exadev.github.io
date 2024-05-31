3d920ddc406315c40ef7974887da5b36f4ad5fd9
Joseph mearman <joseph@mearman.co.uk>
2024-05-31 15:26:04 +0100

    chore: Update page title and base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "ExaDev Breadboard Activity",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "exadev.github.io/Breadboard-Activity",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
