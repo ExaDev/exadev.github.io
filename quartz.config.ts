67c54cde7067cb3f4d4ec1b8f352e0960ceb6fab
Joseph mearman <joseph@mearman.co.uk>
2024-06-14 20:43:38 +0100

    chore: Enable ObsidianFlavoredMarkdown in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -63,7 +63,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
