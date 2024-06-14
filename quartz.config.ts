74e7b1e93a1145b44b3cd589082deb3e849bf73b
Joseph mearman <joseph@mearman.co.uk>
2024-06-14 19:19:14 +0100

    chore: Update defaultDateType to "modified" in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "exadev.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
