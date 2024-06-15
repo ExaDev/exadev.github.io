6e8199bef37706afcbea6d19c04e00c161f59673
Joseph mearman <joseph@mearman.co.uk>
2024-06-15 09:25:02 +0100

    add folderClickBehavior: "link"

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        folderClickBehavior: "link",
+      }),
+    ),
   ],
   right: [
     Component.Graph({
@@ -41,7 +45,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-};
+}
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
